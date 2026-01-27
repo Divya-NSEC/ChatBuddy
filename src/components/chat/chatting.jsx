5d92d9d4030c7d3a77c4eb784a2b1ac944014e9c
Divya Rana <dr316541@gmail.com>
2026-01-28 03:17:45 +0530

    fetching api

--- src/components/chat/chatting.jsx
+++ src/components/chat/chatting.jsx
@@ -58,7 +58,7 @@ const ComponentA = () => {
     setMessage("");
 
     try {
-      const response = await fetch(" https://chat-backend-uogb.onrender.com", {
+      const response = await fetch(" https://chat-backend-uogb.onrender.com/api/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
